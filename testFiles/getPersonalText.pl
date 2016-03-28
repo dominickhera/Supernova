14bb5c2ee7dc3ff2d37ea23151b68d83e1c9f3f1
dominickhera <dominickhera@me.com>
2016-03-28 14:41:12 -0400

    starting to write script that will create the required text files for each script based on what fields you need, NOT WORKING YET

---
+++ testFiles/getPersonalText.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+#
+#   Packages and modules
+#
+use strict;
+use warnings;
+use version;   our $VERSION = qv('5.16.0');   # This is the version of Perl to be used
+use Text::CSV  1.32; 
