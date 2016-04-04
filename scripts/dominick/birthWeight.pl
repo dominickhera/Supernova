400d63713b0db2390874a8a404f45c7c213ab64c
dominickhera <dominickhera@me.com>
2016-04-04 15:07:49 -0400

    added to README

--- scripts/dominick/birthWeight.pl
+++ scripts/dominick/birthWeight.pl
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use version;   our $VERSION = qv('5.16.0');   # This is the version of Perl to be used
 use Text::CSV  1.32;   # We will be using the CSV module (version 1.32 or higher)
-use Term::ProgressBar;
+# use Term::ProgressBar;
 
 
 my $EMPTY = q{};
