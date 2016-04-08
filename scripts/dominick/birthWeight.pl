3e65680ffe4db95e52e7c23f11492981d64c5dd2
dominickhera <dominickhera@me.com>
2016-04-08 09:45:27 -0400

    removed some files i forgot to get rid of

--- scripts/dominick/birthWeight.pl
+++ scripts/dominick/birthWeight.pl
@@ -7,8 +7,8 @@ use warnings;
 use version;   our $VERSION = qv('5.16.0');   # This is the version of Perl to be used
 use Text::CSV  1.32;   # We will be using the CSV module (version 1.32 or higher)
 # use Term::ProgressBar;
- use Statistics::R;
- require "./plot.pl";
+use Statistics::R;
+require "./plot.pl";
 
 
 my $EMPTY = q{};
