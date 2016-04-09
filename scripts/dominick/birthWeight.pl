73f4b128cd7cb64439b6323308aef924e3ac6e28
dominickhera <dominickhera@me.com>
2016-04-09 04:44:22 -0400

    adjusted plot.pl location

--- scripts/dominick/birthWeight.pl
+++ scripts/dominick/birthWeight.pl
@@ -8,7 +8,7 @@ use version;   our $VERSION = qv('5.16.0');   # This is the version of Perl to b
 use Text::CSV  1.32;   # We will be using the CSV module (version 1.32 or higher)
 # use Term::ProgressBar;
 use Statistics::R;
-require "./plot.pl";
+require "./src/plot.pl";
 
 #
 # Author: Dominick Hera
